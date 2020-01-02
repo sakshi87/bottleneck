3a5ba7fa062e65efe7e5b8dff60a815fffbcd0b0
sakshi87 <53863764+sakshi87@users.noreply.github.com>
2020-01-02 15:05:10 +0530

    Update conda_setup.sh

--- tools/travis/conda_setup.sh
+++ tools/travis/conda_setup.sh
@@ -4,7 +4,10 @@ set -ev # exit on first error, print commands
 
 CONDA_URL="http://repo.continuum.io/miniconda"
 
-if [ "${PYTHON_VERSION:0:1}" == "2" ]; then
+if [ `uname -m` = 'aarch64' ]; then
+   MINICONDA_DIR="$HOME/archiconda3"
+   IS_SUDO="sudo"
+elif [ "${PYTHON_VERSION:0:1}" == "2" ]; then
     CONDA="Miniconda2"
 else
     CONDA="Miniconda3"
@@ -14,7 +17,9 @@ if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
 else
     CONDA_OS="Linux"
 fi
-if [ "${PYTHON_ARCH}" == "64" ]; then
+if [ `uname -m` = 'aarch64' ]; then
+   wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
+elif [ "${PYTHON_ARCH}" == "64" ]; then
     URL="${CONDA_URL}/${CONDA}-latest-${CONDA_OS}-x86_64.sh"
 else
     URL="${CONDA_URL}/${CONDA}-latest-${CONDA_OS}-x86.sh"
@@ -25,8 +30,22 @@ set +e
 travis_retry wget "${URL}" -O miniconda.sh
 set -e
 
-chmod +x miniconda.sh
-./miniconda.sh -b -p "${HOME}/miniconda"
+if [ `uname -m` = 'aarch64' ]; then
+   chmod +x archiconda.sh
+   $IS_SUDO apt-get install python-dev
+   $IS_SUDO apt-get install python3-pip
+   $IS_SUDO apt-get install lib$ARCHICONDA_PYTHON-dev
+   $IS_SUDO apt-get install xvfb
+   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/local/lib:/usr/local/bin/python
+   ./archiconda.sh -b
+   echo "chmod MINICONDA_DIR"
+   $IS_SUDO chmod -R 777 $MINICONDA_DIR
+   $IS_SUDO cp $MINICONDA_DIR/bin/* /usr/bin/
+   $IS_SUDO rm /usr/bin/lsb_release
+else
+    chmod +x miniconda.sh
+    ./miniconda.sh -b -p "${HOME}/miniconda"
+    
 export PATH="${HOME}/miniconda/bin:${PATH}"
 hash -r
 conda config --set always_yes yes --set changeps1 no
