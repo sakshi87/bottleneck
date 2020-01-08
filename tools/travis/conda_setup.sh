020d48181860ba1e4c08c2f1887ea800dc2b65bf
sakshi87 <53863764+sakshi87@users.noreply.github.com>
2020-01-08 14:22:58 +0530

    Update conda_setup.sh

--- tools/travis/conda_setup.sh
+++ tools/travis/conda_setup.sh
@@ -37,8 +37,8 @@ set -e
 
 if [ `uname -m` == 'aarch64' ]; then
     chmod +x archiconda.sh
-    $IS_SUDO pip install numpy
     $IS_SUDO apt-get install python-dev
+    $IS_SUDO pip install numpy
     bash archiconda.sh -b -p $HOME/miniconda;
     $IS_SUDO cp -r $HOME/miniconda/bin/* /usr/bin/;
     $IS_SUDO rm /usr/bin/lsb_release
