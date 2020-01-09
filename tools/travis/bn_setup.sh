9866f33b053b6b13bbb10f5a06ab839fff4654fa
sakshi87 <53863764+sakshi87@users.noreply.github.com>
2020-01-09 11:18:48 +0530

    Update bn_setup.sh

--- tools/travis/bn_setup.sh
+++ tools/travis/bn_setup.sh
@@ -19,8 +19,9 @@ else
         pip install "${ARCHIVE[0]}"
     elif [ "${TEST_RUN}" != "coverage" ]; then
         # CFLAGS gets ignored by PEP 518, so do coverage from inplace build
-        pip install "."
+        pip install --user "."
     fi
+    sudo chmod -R 777 /home/travis/miniconda
     python --user setup.py build_ext --inplace
     if [ "${TEST_RUN}" == "doc" ]; then
         make doc
