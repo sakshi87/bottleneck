c4197dec68798e6955dc52986e4c5167926a0df3
sakshi87 <53863764+sakshi87@users.noreply.github.com>
2020-01-21 09:50:28 +0530

    Update conda_setup.sh

--- tools/travis/conda_setup.sh
+++ tools/travis/conda_setup.sh
@@ -41,7 +41,6 @@ if [ `uname -m` == 'aarch64' ]; then
     $IS_SUDO apt-get install python3-pip
     $IS_SUDO pip install numpy
     bash archiconda.sh -b -p $HOME/miniconda;
-    $IS_SUDO pip install Cython
     $IS_SUDO chmod -R 777 /home/travis/.cache/pip
     $IS_SUDO cp -r $HOME/miniconda/bin/* /usr/bin/;
     $IS_SUDO rm /usr/bin/lsb_release
